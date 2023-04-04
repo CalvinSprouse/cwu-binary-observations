022392d100b4cda0787a91733b8b64e4e7047dde
Calvin <sprousecal@gmail.com>
2023-04-04 13:22:03 -0700

    ready for testing

---
+++ Initializer.py
@@ -0,0 +1,99 @@
+# this script creates connection objects from the ACE interface
+# it really serves no purpose to run this alone
+# Python 2
+
+
+# imports
+from __future__ import print_function
+
+import ace.syscore
+import ace.telescope
+import ace.camera
+import ace.dome
+import ace.filterwheel
+import ace.focuser
+
+
+# define a function to create a connection to the ACE interface
+def connect():
+    conn = ace.syscore.AceConnection("localhost")
+    conn.authenticate("root", "password")
+    print("AceConnection Authenticated {0}".format(conn))
+
+    # create a connection to parts of the telescope
+    telescope = ace.telescope.Telescope(conn, "telescope", "Telescope")
+    print("Telescope Connected {0}".format(telescope))
+
+    camera = ace.camera.Camera(conn, "telescope", "FLI Camera")
+    print("Camera Connected {0}".format(camera))
+
+    filterwheel = ace.filterwheel.FilterWheel(conn, "telescope", "Filter Wheel")
+    print("Filter Wheel Connected {0}".format(filterwheel))
+
+    focuser = ace.focuser.Focuser(conn, "telescope", "Focuser")
+    print("Focuser Connected {0}".format(focuser))
+
+    # dome = ace.dome.Dome(conn, "telescope", "Dome")
+    # print("Dome Connected {0}".format(dome))
+    return conn, telescope, camera, filterwheel, focuser
+
+
+# define functions to output information on telescope parts
+def get_telescope_info(telescope_conn: ace.telescope.Telescope) -> dict:
+    return {
+        "target": telescope_conn.get_target(),
+        "position": telescope_conn.get_position(),
+    }
+
+def get_camera_info(camera_conn: ace.camera.Camera) -> dict:
+    return {
+        "state": camera_conn.state,
+        "temperature": camera_conn.temperature,
+        "target_temperature": camera_conn.setpoint,
+        "readout_mode": camera_conn.readout_mode,
+        "can_pause": camera_conn.can_pause,
+    }
+
+def get_filterwheel_info(filterwheel_conn: ace.filterwheel.FilterWheel) -> dict:
+    return {
+        "state": filterwheel_conn.state,
+        "position": filterwheel_conn.position,
+        "positions": filterwheel_conn.positions,
+        "filter_name": filterwheel_conn.filter_name,
+        "target": filterwheel_conn.target,
+        "names": filterwheel_conn.get_names(),
+    }
+
+def get_dome_info(dome_conn: ace.dome.Dome) -> dict:
+    return {
+        "state": dome_conn.state,
+        "azimuth": dome_conn.azimuth,
+        "target_azimuth": dome_conn.cmd_azimuth,
+        "target_elevation": dome_conn.cmd_elevation,
+        "park_azimuth": dome_conn.park_azimuth,
+    }
+
+def get_focuser_info(focuser_conn: ace.focuser.Focuser) -> dict:
+    return {
+        "state": focuser_conn.state,
+        "position": focuser_conn.position,
+        "target": focuser_conn.target,
+        "minimum": focuser_conn.minimum,
+        "maximum": focuser_conn.maximum,
+    }
+
+
+# load focuser values
+# update each night
+filter_focus_dict = {
+    "Clear": 0,
+    "Bessel U": 0,
+    "Bessel B": 0,
+    "Bessel V": 0,
+    "Bessel R": 0,
+    "Bessel I": 0,
+    "Halpha": 0,
+    "Hbeta": 0,
+    "O3": 0,
+    "O2": 0,
+}
---
+++ MultiTargetObservation.py
@@ -0,0 +1,95 @@
+# this script runs automated observations of multiple targets, filters, and exposures
+
+# imports
+from __future__ import print_function
+
+import time
+
+# import astropy
+# import astropy.coordinates as coord
+
+from Initializer import *
+
+from astropy.coordinates import SkyCoord
+
+# get connection objects
+conn, telescope, camera, filterwheel, focuser = connect()
+
+# define the observation orders
+# orders are a dictionary of the following format
+# ensure filters are the same as in initializer
+# note that pause interval happens after exposure so the full elapsed time
+# is exptime + readout_time + pause_interval
+# before taking a new picture
+# readouttime cannot be specified
+observe_44_boo = {
+    "target_name": "44 Boo",
+    "file_name_prefix": "44Boo",
+
+    "exposure_orders": {
+        "Bessel U": {
+            "count": 10,
+            "exptime_seconds": 10,
+            "pause_interval_seonds": 60,
+            "last_exposure": 0
+            },
+    }
+}
+
+# load all observation orders into a list
+observation_list = [
+    observe_44_boo,
+]
+
+# test each observation order and sum the image counts
+image_counts = 0
+for index, observe in enumerate(observation_list):
+    target_pos = SkyCoord.from_name(observe["target_name"])
+    print("Position of {0} is {1}".format(
+        observe["target_name"], target_pos))
+    observation_list[index]["target_pos"] = target_pos
+
+    for key, filter in observe["exposure_orders"].items():
+        image_counts += filter["count"]
+
+# begin observation loop
+for obs_index in range(image_counts):
+    print("Observation Index {0} of {1}".format(obs_index, image_counts))
+
+    # find the next image that needs taking by scanning the observe orders for the lowest time to image
+    lowest_time_to_image = time.time()
+    next_index = (0, 0)
+
+    for o_index, observe in enumerate(observation_list):
+        for key, filter in observe["exposure_orders"].items():
+            if filter["count"] <= 0: continue
+
+            if time.time() - filter["last_exposure"]:
+                lowest_time_to_image = time.time() - filter["last_exposure"]
+                next_index = (o_index, key)
+
+    print("Nearest image found to be in {0} seconds for {1}".format(
+        lowest_time_to_image,
+        observation_list[next_index[0]]["target_name"]
+    ))
+
+    # orient telescope to next target
+    telescope.go_to_j2000(observation_list[next_index[0]]["target_pos"].ra.degree,
+                          observation_list[next_index[0]]["target_pos"].dec.degree)
+
+    # set the file format and load values from dict
+    camera.template = observation_list[next_index[0]]["target_name_prefix"] + [next_index[1].replace(" ", "-")] + "_PY_{{seq:3}}.fits"
+    exptime = observation_list[next_index[0]]["exposure_orders"][next_index[1]]["exptime_seconds"]
+
+    # wait until ready
+    while time.time() - observation_list[next_index[0]]["exposure_orders"][next_index[1]]["last_exposure"] < observation_list[next_index[0]]["exposure_orders"][next_index[1]]["pause_interval_seconds"]:
+        time.sleep(0.1)
+
+    # expose
+    print("Exposing for {0} seconds to {1}".format(exptime, camera.template))
+    camera.expose(exptime)
+    print("Exposure complete saved to {0}".format(camera.template))
+
+    # reduce img count and set last exposure
+    observation_list[next_index[0]]["exposure_orders"][next_index[1]]["count"] -= 1
+    observation_list[next_index[0]]["exposure_orders"][next_index[1]]["last_exposure"] = time.time()
