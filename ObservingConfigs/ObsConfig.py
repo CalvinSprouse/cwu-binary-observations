aa2e27e9bbced16d61ba2a398801fd688071bf37
Calvin <sprousecal@gmail.com>
2023-05-24 23:20:13 -0700

    updated obsconfig

--- ObservingConfigs/ObsConfig.py
+++ ObservingConfigs/ObsConfig.py
@@ -7,55 +7,183 @@ obs_loop = 0
 # obs_time controls the maximum duration to run the observation in hours
 # must be some non-zero number (failsafe)
 # 2 hours is a good limit beyond which the lenses may require re-focusing
-obs_time = 1
+obs_time = 2
 
 # obs_pause controls how long to pause between loops in seconds
-obs_pause = 5*60
+obs_pause = 1*60
 
 # configure the focus wheel values
 # add in any filter that will be used as a key
 # and the value of the focus wheel as a value
 filter_focus_dict = {
-    "Bessel B": -30763,
-    "Bessel V": -29185,
-    "Bessel R": -29167,
+    "Bessel B": -9609,
+    "Bessel V": -10671,
+    "Bessel R": -11161,
 }
 
 # obs_list defines the observations to do, the obs_list will be repeated
 # as per obs_loop and run for obs_time (whichever expires first)
 # each element of obs_list is an "observation" and is a dictionary
-basic_pause = 30
-img_count = 3
+basic_pause = 5
+img_count = 1
 obs_list = [
-#    {
-#        "target_name": "W UMa",
-#        "filter_name": "Bessel V",
-#        "exp_time": 8,
-#        "img_count": img_count,
-#        "pause_time": basic_pause,
-#    },
-
-#    {
-#        "target_name": "W UMa",
-#        "filter_name": "Bessel B",
-#        "exp_time": 16,
-#        "img_count": img_count,
-#        "pause_time": basic_pause,
-#    },
-
-#   {
-#       "target_name": "W UMa",
-#       "filter_name": "Bessel R",
-#       "exp_time": 4,
-#       "img_count": img_count,
-#       "pause_time": basic_pause,
-#   },
+    {
+        "target_name": "BZ Boo",
+        "filter_name": "Bessel V",
+        "exp_time": 25,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
 
     {
-	"target_name": "BZ Boo",
-	"filter_name": "Bessel V",
-	"exp_time": 8,
-	"img_count": img_count,
-	"pause_time": basic_pause,
+        "target_name": "BZ Boo",
+        "filter_name": "Bessel V",
+        "exp_time": 15,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V417 Boo",
+        "filter_name": "Bessel V",
+        "exp_time": 40,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V417 Boo",
+        "filter_name": "Bessel V",
+        "exp_time": 20,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V336 Boo",
+        "filter_name": "Bessel V",
+        "exp_time": 180,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V336 Boo",
+        "filter_name": "Bessel V",
+        "exp_time": 90,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V462 Dra",
+        "filter_name": "Bessel V",
+        "exp_time": 90,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V462 Dra",
+        "filter_name": "Bessel V",
+        "exp_time": 45,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "MX UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 20,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "MX UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 10,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V384 UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 120,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V384 UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 60,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V398 UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 180,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "V398 UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 90,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "W UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 20,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "W UMa",
+        "filter_name": "Bessel V",
+        "exp_time": 10,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "W UMa",
+        "filter_name": "Bessel B",
+        "exp_time": 60,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "W UMa",
+        "filter_name": "Bessel B",
+        "exp_time": 30,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "W UMa",
+        "filter_name": "Bessel R",
+        "exp_time": 8,
+        "img_count": img_count,
+        "pause_time": basic_pause,
+    },
+
+    {
+        "target_name": "W UMa",
+        "filter_name": "Bessel R",
+        "exp_time": 4,
+        "img_count": img_count,
+        "pause_time": basic_pause,
     },
 ]
